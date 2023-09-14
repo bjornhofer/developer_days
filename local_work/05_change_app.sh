8736b98443228542dd70db192e166aa93f0d6d66
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-09-14 15:51:37 +0200

    Update 05_change_app.sh

--- local_work/05_change_app.sh
+++ local_work/05_change_app.sh
@@ -15,9 +15,8 @@ docker compose up --build -d
 docker tag mcr.microsoft.com/azuredocs/azure-vote-front:v1 auvadevloperdays.azurecr.io/azure-vote-front:v2
 docker push auvadevloperdays.azurecr.io/azure-vote-front:v2
 
-read -p "Image Tags changed to v2 and pushed to Azure Container Registry" -n1 -s
+read -p "Press any key to continue... " -n1 -s
 
 kubectl set image deployment azure-vote-front azure-vote-front=auvadevloperdays.azurecr.io/azure-vote-front:v2
 
-
-kubectl get pods
+watch -n 1 kubectl get pods
