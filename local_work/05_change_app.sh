d455e4d14345e68eaaaa454edbae5039246cba1a
Björn Andreas Höfer <bjoern.hoefer@microsoft.com>
2023-09-14 14:52:30 +0200

    fixture

--- local_work/05_change_app.sh
+++ local_work/05_change_app.sh
@@ -9,7 +9,7 @@ clear
 vi /home/azuser/git/azure-voting-app-redis/azure-vote/azure-vote/config_file.cfg
 read -p "Press any key to continue... " -n1 -s
 
-
+cd /home/azuser/git/azure-voting-app-redis/
 docker compose up --build -d
 
 docker tag mcr.microsoft.com/azuredocs/azure-vote-front:v1 auvadevloperdays.azurecr.io/azure-vote-front:v2
